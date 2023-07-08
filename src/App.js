48f28c0c7da96dd0fe1a120a96b32dc3279e09cf
Anil Pandiya <anilpandiya05@gmail.com>
2023-07-08 12:27:07 +0530

    setup | basic setup for fiber app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh rotation={[1, 0, 1]} position={[0,1,2]}>
+          <octahedronGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
